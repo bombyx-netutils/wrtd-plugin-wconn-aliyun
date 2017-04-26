5932965430c6e050d9584588074080057fe637a6
fpemud <fpemud@sina.com>
2017-04-26 14:28:25 +0000

    fix

--- wconn_aliyun/__init__.py
+++ wconn_aliyun/__init__.py
@@ -47,24 +47,24 @@ class _PluginObject:
         if ifname == "eth0":
             if "intranet" in self.cfg:
                 ip, netmask = self.cfg["intranet"]["ip"].split("/")
-                with pyroute2.IPRoute() as ip:
-                    idx = ip.link_lookup(ifname="eth0")[0]
-                    ip.link("set", index=idx, state="up")
-                    ip.addr("add", index=idx, address=ip, mask=netmask)
+                with pyroute2.IPRoute() as ipp:
+                    idx = ipp.link_lookup(ifname="eth0")[0]
+                    ipp.link("set", index=idx, state="up")
+                    ipp.addr("add", index=idx, address=ip, mask=_Util.ipMaskToLen(netmask))
                     if "routes" in self.cfg["intranet"]:
                         for rt in self.cfg["intranet"]["routes"]:
-                            ip.route('add', dst=rt["prefix"], gateway=rt["gateway"], oif=idx)
+                            ipp.route('add', dst=rt["prefix"], gateway=rt["gateway"], oif=idx)
             logging.info("WAN: Internet interface \"%s\" is managed." % (ifname))
             return True
 
         if ifname == "eth1":
             ip, netmask = self.cfg["internet"]["ip"].split("/")
-            with pyroute2.IPRoute() as ip:
-                idx = ip.link_lookup(ifname="eth1")[0]
-                ip.link("set", index=idx, state="up")
-                ip.addr("add", index=idx, address=ip, mask=netmask)
+            with pyroute2.IPRoute() as ipp:
+                idx = ipp.link_lookup(ifname="eth1")[0]
+                ipp.link("set", index=idx, state="up")
+                ipp.addr("add", index=idx, address=ip, mask=_Util.ipMaskToLen(netmask))
                 if "gateway" in self.cfg["internet"]:
-                    ip.route('add', dst="0.0.0.0/0", gateway=self.cfg["internet"]["gateway"], oif=idx)
+                    ipp.route('add', dst="0.0.0.0/0", gateway=self.cfg["internet"]["gateway"], oif=idx)
             logging.info("WAN: Internet interface \"%s\" is managed." % (ifname))
             return True
 
@@ -72,3 +72,17 @@ class _PluginObject:
 
     def interface_disappear(self, ifname):
         pass
+
+
+class _Util:
+
+    @staticmethod
+    def ipMaskToLen(mask):
+        """255.255.255.0 -> 24"""
+
+        netmask = 0
+        netmasks = mask.split('.')
+        for i in range(0, len(netmasks)):
+            netmask *= 256
+            netmask += int(netmasks[i])
+        return 32 - (netmask ^ 0xFFFFFFFF).bit_length()
